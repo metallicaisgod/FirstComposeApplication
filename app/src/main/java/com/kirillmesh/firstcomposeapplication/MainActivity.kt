bdd1d45fe54088e6741be2f4191aef47bc4c0072
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-06-21 12:31:31 +0300

    Initial commit

---
+++ app/src/main/java/com/kirillmesh/firstcomposeapplication/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.kirillmesh.firstcomposeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TimesTable()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1..9) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1..9) {
+                    val color = if ((j + i) % 2 == 0) {
+                        Color.Yellow
+                    } else {
+                        Color.White
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .border(width = 1.dp, color = Color.DarkGray)
+                            .background(color),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "${i * j}")
+                    }
+                }
+            }
+        }
+    }
+}
