31d5dd03d2232d25b0fb8e8cd5444df113c5d443
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-06-21 16:44:08 +0300

    Added elements

--- app/src/main/java/com/kirillmesh/firstcomposeapplication/ui/compose/InstagramCard.kt
+++ app/src/main/java/com/kirillmesh/firstcomposeapplication/ui/compose/InstagramCard.kt
@@ -3,14 +3,24 @@ package com.kirillmesh.firstcomposeapplication.ui.compose
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontFamily
@@ -33,41 +43,56 @@ fun InstagramCard() {
         modifier = Modifier
             .padding(4.dp)
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(50.dp)
+        Column(modifier = Modifier.padding(10.dp)) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
             ) {
+
                 Image(
                     imageVector =
                     ImageVector.vectorResource(id = R.drawable.instagram_icon),
                     contentDescription = "Instagram icon",
                     alignment = Alignment.Center,
                     modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                        .background(Color.White)
                         .padding(5.dp)
                 )
+                InstagramCardColumns(
+                    topText = "6,950",
+                    bottomText = "Posts",
+                    modifier = Modifier
+                )
+                InstagramCardColumns(
+                    topText = "436M",
+                    bottomText = "Followers",
+                    modifier = Modifier
+                )
+                InstagramCardColumns(
+                    topText = "76",
+                    bottomText = "Following",
+                    modifier = Modifier
+                )
             }
-
-            InstagramCardColumns(
-                topText = "6,950",
-                bottomText = "Posts",
-                modifier = Modifier
+            Text(
+                text = "Instagram",
+                fontSize = 32.sp,
+                fontFamily = FontFamily.Cursive
             )
-            InstagramCardColumns(
-                topText = "436M",
-                bottomText = "Followers",
-                modifier = Modifier
+            Text(
+                text = "#YoursToMake",
+                fontSize = 14.sp
             )
-            InstagramCardColumns(
-                topText = "76",
-                bottomText = "Following",
-                modifier = Modifier
+            Text(
+                text = "www.facebook.com/emotional_health",
+                fontSize = 14.sp
             )
+            Button(onClick = { }) {
+                Text(text = "Follow")
+            }
         }
     }
 }
@@ -86,12 +111,12 @@ private fun InstagramCardColumns(
     ) {
         Text(
             text = topText,
-            fontSize = 30.sp,
+            fontSize = 32.sp,
             fontFamily = FontFamily.Cursive
         )
         Text(
             bottomText,
-            fontSize = 12.sp,
+            fontSize = 14.sp,
             fontWeight = FontWeight.Bold
         )
     }
@@ -104,7 +129,6 @@ fun PreviewCardLight() {
         darkTheme = false
     ) {
         InstagramCardInBox()
-
     }
 }
 
