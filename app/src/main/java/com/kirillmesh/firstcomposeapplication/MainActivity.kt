baa0ec9760f00c2b064afcda5147cb15e962c9c7
Kirill Meshkov <metallicaisgod@yandex.ru>
2023-06-21 15:26:42 +0300

    Dark-Light Themes

--- app/src/main/java/com/kirillmesh/firstcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/kirillmesh/firstcomposeapplication/MainActivity.kt
@@ -3,18 +3,28 @@ package com.kirillmesh.firstcomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
 import com.kirillmesh.firstcomposeapplication.ui.compose.InstagramCard
+import com.kirillmesh.firstcomposeapplication.ui.theme.FirstComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstagramCard()
+            FirstComposeApplicationTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                ) {
+                    InstagramCard()
+                }
+            }
         }
     }
 }
-
-
-
-
 
--- app/src/main/java/com/kirillmesh/firstcomposeapplication/ui/compose/InstagramCard.kt
+++ app/src/main/java/com/kirillmesh/firstcomposeapplication/ui/compose/InstagramCard.kt
@@ -1,55 +1,74 @@
 package com.kirillmesh.firstcomposeapplication.ui.compose
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kirillmesh.firstcomposeapplication.R
+import com.kirillmesh.firstcomposeapplication.ui.theme.FirstComposeApplicationTheme
 
-@Preview(showBackground = true)
 @Composable
 fun InstagramCard() {
-    Row(
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colors.onBackground),
+        backgroundColor = MaterialTheme.colors.background,
         modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+            .padding(4.dp)
     ) {
-        Box(modifier = Modifier
-            .size(50.dp)){
-            Image(
-                imageVector =
-                ImageVector.vectorResource(id = R.drawable.instagram_icon),
-                contentDescription = "Instagram icon",
-                alignment = Alignment.Center,
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
                 modifier = Modifier
-                    .padding(5.dp)
+                    .size(50.dp)
+            ) {
+                Image(
+                    imageVector =
+                    ImageVector.vectorResource(id = R.drawable.instagram_icon),
+                    contentDescription = "Instagram icon",
+                    alignment = Alignment.Center,
+                    modifier = Modifier
+                        .padding(5.dp)
+                )
+            }
+
+            InstagramCardColumns(
+                topText = "6,950",
+                bottomText = "Posts",
+                modifier = Modifier
+            )
+            InstagramCardColumns(
+                topText = "436M",
+                bottomText = "Followers",
+                modifier = Modifier
+            )
+            InstagramCardColumns(
+                topText = "76",
+                bottomText = "Following",
+                modifier = Modifier
             )
         }
-
-        InstagramCardColumns(
-            topText = "6,950",
-            bottomText = "Posts",
-            modifier = Modifier
-        )
-        InstagramCardColumns(
-            topText = "436M",
-            bottomText = "Followers",
-            modifier = Modifier
-        )
-        InstagramCardColumns(
-            topText = "76",
-            bottomText = "Following",
-            modifier = Modifier
-        )
     }
 }
 
@@ -58,23 +77,54 @@ private fun InstagramCardColumns(
     topText: String,
     bottomText: String,
     modifier: Modifier = Modifier
-){
+) {
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceEvenly
 
-    ){
-        Box(contentAlignment = Alignment.Center){
-            Text(
-                text = topText,
-                fontSize = 30.sp,
-                fontStyle = FontStyle.Italic
-            )
-        }
-        Box(contentAlignment = Alignment.Center){
-            Text(bottomText)
-        }
+    ) {
+        Text(
+            text = topText,
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Cursive
+        )
+        Text(
+            bottomText,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
 
+@Preview
+@Composable
+fun PreviewCardLight() {
+    FirstComposeApplicationTheme(
+        darkTheme = false
+    ) {
+        InstagramCardInBox()
+
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardDark() {
+    FirstComposeApplicationTheme(
+        darkTheme = true
+    ) {
+        InstagramCardInBox()
+    }
+}
+
+@Composable
+private fun InstagramCardInBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colors.background)
+    ) {
+        InstagramCard()
     }
 }
